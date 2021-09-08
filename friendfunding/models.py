fd07a71fd06eb6faf666589824c8feb2d564c77f
ynnabaina <ynnasalzw@gmail.com>
2021-09-07 17:07:26 -0700

    pushing to main, USER AUTHEN

--- friendfunding/models.py
+++ friendfunding/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    goal = models.CharField(max_length=100)
+    description = models.TextField()
+    amountsaved = models.IntegerField()
+    print("The User Model")
+    
+    def __str__(self):
+        return self.name
+
+class Goal(models.Model):
+
+    title = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='goal',
+    )
+    description = models.TextField()
+    cost = models.IntegerField()
+    amountsaved = models.IntegerField()
+
+
+    # Can use ForeignKey to bring in User model
+    # Can also add multiple users to a specific Goal. I imagine it could do something with an array of users..
+    user = models.CharField(max_length=100)
+    # Would want to pull the amount saved from each user.... 
+    print("The Goal Model")
+
+    def __str__(self):
+        return self.title
+
--- friendfunding/views.py
+++ friendfunding/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_create(request):
+    if request.method == 'POST':
+        form = userForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('user_detail', pk=user.pk)
+        else:
+            form = userForm()
+    else:
+        form = userForm()
+    return render(request, 'tunr/user_form.html', {'form':form})
+
+def user_edit(request, pk):
+    user = user.objects.get(k=pk) #Todo.findById(matchXXX)
+    if request.method == "POST":
+        form = userForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            return redirect('user_detail', pk=user.pk)
+    else:
+        form = userForm(instance=user)
+    return render(request, 'tunr/user_form.html', {'form': form})
+
+def user_delete(request, pk):
+    user.objects.get(id=pk).delete()
+    return redirect('user_list')
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'tunr/song_list.html', {'songs': songs})
+
+def song_detail(request, pk):
+    song = Song.objects.get(id=pk)
+    return render(request, 'tunr/song_detail.html', {'song': song})
+
+def user_detail(request, pk):
+    
+    try:
+        user = user.objects.get(id=pk)
+    except:
+        user = {
+            'name': "No user found", 
+            'nationality': f'with id {pk}'
+            }
+        print(f"user with id={pk} didn't work")
+    
+    return render(request, 'tunr/user_detail.html', {'user': user})
